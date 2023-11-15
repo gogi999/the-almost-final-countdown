01139022903626a840fa3d24ec104cd18d793f42
gogi999 <g.petrovic9@gmail.com>
2023-11-15 11:44:34 +0100

    Added basic setup and introduced refs

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import {
+  useRef,
+  useState,
+} from 'react';
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+  /*
+  function handleChange(e) {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);
+  }
+  */
+  function handleClick(e) {
+    // setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+  }
+
+  return (
+    <section id="player">
+      <h2>
+        Welcome {/*submitted*/ enteredPlayerName ?? 'unknown entity'}
+      </h2>
+      <p>
+        <input 
+          ref={playerName}
+          type="text" 
+          // onChange={handleChange} 
+          // value={enteredPlayerName}  
+        />
+        <button onClick={handleClick}>
+          Set Name
+        </button>
+      </p>
+    </section>
+  );
+}
